ac57edcd8395385774d83e7cc9ea0be6a6f99d04
Ramon Barbosa <ramonmfb777@gmail.com>
2021-07-12 17:11:48 -0300

    Project created

---
+++ src/main/java/com/studyproject/dsproject/DsprojectApplication.java
@@ -0,0 +1,13 @@
+package com.studyproject.dsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsprojectApplication.class, args);
+	}
+
+}
